d25f6aa8552d6cc2092ce7ef8cd073e841196601
Inventory <changoamd@gmail.com>
2026-02-26 23:09:09 +0000

    config excluded

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['74.208.126.208', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['inventory.natuvu.org', 'localhost', '127.0.0.1']
 
 
 # Application definition
